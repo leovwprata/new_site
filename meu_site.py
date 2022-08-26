bb25461e1b45fecb263e49c08bad894ce0cdded0
Liinformatica <leojorgerj@gmail.com>
2022-08-26 13:53:19 -0300

    Alteração1

--- meu_site.py
+++ meu_site.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 # route -> hashtagtreinamentos.com/
 # função -> o que você quer exibir naquela página
 # template
@@ -9,18 +11,20 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
+
 @app.route("/contatos")
 def contatos():
     return render_template("contatos.html")
 
+
 @app.route("/usuarios/<nome_usuario>")
-def usuarios (nome_usuario):
-    return render_template ("usuarios.html", nome_usuario=nome_usuario)
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
 
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
 
     # servidor do heroku
-
-
+    # teste1
